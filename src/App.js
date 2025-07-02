9da40cb715cb3a7d71dc91c1ebdacc3fa23da6a1
Aryan Pande <aryanpande01@gmail.com>
2025-07-02 20:19:05 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Box from './components/Box';
+import Nav from './components/Nav';
+import Foot from './components/Foot';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+      <Box/>
+      <Foot/>
+    </div>
+  );
+};
+
+export default App;
